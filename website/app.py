3005ec713f1733bf8f30862dbf053f4bcd00b658
Brandon <brandonesser435@gmail.com>
2023-11-03 02:33:33 -0700

    Change lottery wording and formatting, possible typeError fix.

--- website/app.py
+++ website/app.py
@@ -136,7 +136,10 @@ def update_lottery():
         ticket_price = int(request.form["ticket_price"])
         draw_date = request.form["draw_date"]
 
-        return 200
+        return "OK", 200
+        #BUG something throws a typeError. Could it be this? Should it be: return "OK", 200"?
+        
+
     except Exception as e:
         logging.error(f"Internal error on update_lottery: {e}")
         return "Internal error on update_lottery", 500
